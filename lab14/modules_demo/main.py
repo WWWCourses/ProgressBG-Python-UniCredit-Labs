bcf9d23411968cc6f6be9e812000453d36abde4b
WWWCourses <ivawebcourses@gmail.com>
2022-02-25 15:21:28 +0200

    Lab14: 25.02.2022 DONE: Virtual Environments

---
+++ lab14/modules_demo/main.py
@@ -0,0 +1,11 @@
+# import module1 as m
+# from module1 import PI,A
+
+import packageA.module1 as m1
+import packageA.module2 as m2
+
+a = m1.A()
+a.greet()
+
+print(m2.PI)
+
---
+++ lab14/modules_demo/packageA/module1.py
@@ -0,0 +1,13 @@
+class A:
+	x = 1
+	def greet(self):
+		print('Hello')
+
+if __name__=='__main__':
+	a = A()
+	print('a is created')
+	a.greet()
+
+
+# print('Module is loaded')
+# print(__name__)
